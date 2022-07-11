da5926d8a5470f58750bc10c8248e2d832a7c90c
qwanchoi <chlrbdhks92@gmail.com>
2022-07-11 14:51:12 +0900

    타임리프 작업 테스트

--- tuna/src/main/java/co/fourth/tuna/web/eclass/professor/EclassProfessorEclassController.java
+++ tuna/src/main/java/co/fourth/tuna/web/eclass/professor/EclassProfessorEclassController.java
@@ -4,17 +4,30 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import co.fourth.tuna.web.eclass.EclassController;
+
 
 @Controller
 @RequestMapping("/eclass/professor")
 public class EclassProfessorEclassController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(EclassController.class);
+	private String path = "/eclass/professor";
 	
-	@RequestMapping("/")
-	public void Eclass() {
-		logger.info("eclass professor test");
+//	private static final Logger logger = LoggerFactory.getLogger(EclassController.class);
+	
+	@RequestMapping(value = {"/", "/home"}, method = RequestMethod.GET)
+	public String homeView() {
+		
+		return path+"/home";
 	}
+	
+	@RequestMapping({"notice"})
+	public String noticeView() {
+		return path+"/notice";
+	}
+	
+	
+	
 }
