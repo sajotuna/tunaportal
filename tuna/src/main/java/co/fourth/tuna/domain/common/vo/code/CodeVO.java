d9f7069435ec8ffab18a2efdc4896e066bb09da6
qwanchoi <chlrbdhks92@gmail.com>
2022-07-11 17:46:22 +0900

    공통코드 - 코드 입력시 이름 출력

---
+++ tuna/src/main/java/co/fourth/tuna/domain/common/vo/code/CodeVO.java
@@ -0,0 +1,15 @@
+package co.fourth.tuna.domain.common.vo.code;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CodeVO {
+	private String no;
+	private String codeName;
+	
+//	private CodeMasterVO parent; // parent
+}
