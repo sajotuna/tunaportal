2475ceac5c6e041098cd5a10eb990cd68c198bba
qwanchoi <chlrbdhks92@gmail.com>
2022-07-11 18:58:44 +0900

    eclass html 페이지 thymeleaf 속성 추가 및 컨트롤러 추가

--- tuna/src/main/java/co/fourth/tuna/web/eclass/professor/EclassProfessorEclassController.java
+++ tuna/src/main/java/co/fourth/tuna/web/eclass/professor/EclassProfessorEclassController.java
@@ -1,32 +1,82 @@
 package co.fourth.tuna.web.eclass.professor;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import co.fourth.tuna.web.eclass.EclassController;
 
 
 @Controller
 @RequestMapping("/eclass/professor")
 public class EclassProfessorEclassController {
 	
-	private String path = "/eclass/professor";
+	//private String profPath = "/eclass/professor";
 	
-//	private static final Logger logger = LoggerFactory.getLogger(EclassController.class);
+	private static final Logger logger = LoggerFactory.getLogger(EclassProfessorEclassController.class);
 	
-	@RequestMapping(value = {"/", "/home"}, method = RequestMethod.GET)
-	public String homeView() {
-		
-		return path+"/home";
+	@GetMapping(value = {"/", "/home"})
+	public String homeView(Model model, HttpServletRequest req) {
+//		logger.info(req.getRequestURI()); //tuna/eclass/professor/notice
+//		logger.info(req.getRequestURL().toString()); //http://localhost/tuna/eclass/professor/notice
+//		logger.info(req.getServletPath()); //eclass/professor/notice
+		return req.getServletPath();
 	}
 	
-	@RequestMapping({"notice"})
-	public String noticeView() {
-		return path+"/notice";
+	@GetMapping("/notice")
+	public String noticeView(Model model, HttpServletRequest req) {
+		return req.getServletPath();
 	}
+	
+	@GetMapping("/noticeForm")
+	public String noticeFormView(Model model, HttpServletRequest req) {
+		return req.getServletPath();
+	}
+	
+	@GetMapping("/noticeList")
+	public String noticeListView(Model model, HttpServletRequest req) {
+		return req.getServletPath();
+	}
+	
+	@GetMapping("/noticeUpdate")
+	public String noticeUpdateView(Model model, HttpServletRequest req) {
+		return req.getServletPath();
+	}
+	
+	@GetMapping("/qna")
+	public String qnaView(Model model, HttpServletRequest req) {
+		return req.getServletPath();
+	}
+	
+	@GetMapping("/qnaList")
+	public String qnaListView(Model model, HttpServletRequest req) {
+		return req.getServletPath();
+	}
+	
+	@GetMapping("/studentList")
+	public String studentListView(Model model, HttpServletRequest req) {
+		return req.getServletPath();
+	}
+	
+	@GetMapping("/subject")
+	public String subjectView(Model model, HttpServletRequest req) {
+		return req.getServletPath();
+	}
+	
+	@GetMapping("/subjectForm")
+	public String subjectFormView(Model model, HttpServletRequest req) {
+		return req.getServletPath();
+	}
+	
+	@GetMapping("/subjectList")
+	public String subjectListView(Model model, HttpServletRequest req) {
+		return req.getServletPath();
+	}
+	
+	
 	
 	
 	
