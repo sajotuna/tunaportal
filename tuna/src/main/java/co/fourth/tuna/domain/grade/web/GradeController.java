8186a86e6300debf5b291c61949acdea5ee08547
Binggla <wlqls12@naver.com>
2022-07-12 10:40:56 +0900

    링크 연결 완료!

--- tuna/src/main/java/co/fourth/tuna/domain/grade/web/GradeController.java
+++ tuna/src/main/java/co/fourth/tuna/domain/grade/web/GradeController.java
@@ -5,5 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class GradeController {
-	
+
+	// 당해학기 성적 조회 페이지 이동
+	@RequestMapping("/stud/currentSemesterGrade")
+	public String currentSemesterGrade() {
+		return "portal/stud/currentSemesterGrade";
+	}
 }
