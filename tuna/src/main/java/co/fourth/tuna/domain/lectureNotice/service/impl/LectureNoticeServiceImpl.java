8e6acb908f8141e3ff750870bbbf484224971ba6
hwachoo <dwg4134@gmail.com>
2022-07-14 18:06:24 +0900

    강의계획서 완료, 강의공지 작업 중

--- tuna/src/main/java/co/fourth/tuna/domain/lectureNotice/service/impl/LectureNoticeServiceImpl.java
+++ tuna/src/main/java/co/fourth/tuna/domain/lectureNotice/service/impl/LectureNoticeServiceImpl.java
@@ -1,7 +1,25 @@
 package co.fourth.tuna.domain.lectureNotice.service.impl;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import co.fourth.tuna.domain.lectureNotice.mapper.LectureNoticeMapper;
 import co.fourth.tuna.domain.lectureNotice.service.LectureNoticeService;
+import co.fourth.tuna.domain.lectureNotice.vo.LectureNoticeVO;
 
 public class LectureNoticeServiceImpl implements LectureNoticeService {
+
+	@Autowired LectureNoticeMapper map;
+	
+	@Override
+	public List<LectureNoticeVO> noticeList(int state, String key) {
+		return map.noticeList(state, key);
+	}
+
+	@Override
+	public LectureNoticeVO noticeSelect(LectureNoticeVO vo) {
+		return map.noticeSelect(vo);
+	}
 
 }
--- tuna/src/main/java/co/fourth/tuna/domain/lectureplan/service/LecturePlanService.java
+++ tuna/src/main/java/co/fourth/tuna/domain/lectureplan/service/LecturePlanService.java
@@ -6,4 +6,7 @@ public interface LecturePlanService {
 
 	//단일과목 강의계획 조회
 	public LecturePlanVO selectPlan(LecturePlanVO vo);
+	
+	//단일과목 강의스케쥴 조회
+	public LecturePlanVO selectSc(LecturePlanVO vo);
 }
