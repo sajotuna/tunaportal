83644161f9624d97eafc108dd475ca37478d40ba
chichi <deletein@naver.com>
2022-07-20 09:20:18 +0900

    오류수정

--- tuna/src/main/java/co/fourth/tuna/domain/lectureplan/web/LecturePlanController.java
+++ tuna/src/main/java/co/fourth/tuna/domain/lectureplan/web/LecturePlanController.java
@@ -30,30 +30,6 @@ public class LecturePlanController {
 	@Autowired private YearService yService;
 	@Autowired private PortalScheduleService scheduleService;
 	
-	@RequestMapping("/portal/student/lecturePlan")
-	@ResponseBody
-	public List<Map<String,Object>> lecturePlanModal(Model model, LecturePlanVO vo) {
-		
-		List<Map<String,Object>> lists = sql.selectList("co.fourth.tuna.domain.lectureplan.mapper.LecturePlanMapper.selectPlan", vo.getSbjNo());
-		List<Map<String,Object>> lectureSchedule = sql.selectList("co.fourth.tuna.domain.lectureplan.mapper.LecturePlanMapper.selectSc", vo.getSbjNo());
-		
-		for (Map<String,Object> ls : lectureSchedule) {
-			lists.add(ls);
-		}
-		
-		String[] plan = lists.get(0).values().toString().split(",");
-		String[] weekplan = plan[11].split("~");
-		
-		Map<String, Object> map = new HashMap<>();
-		
-		for(int i=1; i<=weekplan.length; i++) {
-			map.put(Integer.toString(i), weekplan[(i-1)]);
-		}
-		
-		lists.add(map);
-
-		return lists;
-	}
 	
 	@RequestMapping("/stud/CoursePlan")
 	public String CoursePlan(Model model, LecturePlanVO vo) {
@@ -77,27 +53,7 @@ public class LecturePlanController {
 	}
 	
 	
-	@RequestMapping("/eclass/student/lecturePlan")
-	public String lecturePlan(Model model, LecturePlanVO vo) {
-		vo.setSbjNo(18101);
-		List<Map<String,Object>> lists = sql.selectList("co.fourth.tuna.domain.lectureplan.mapper.LecturePlanMapper.selectPlan", vo.getSbjNo());
-		//강의스케쥴
-		List<Map<String,Object>> lectureSchedule = sql.selectList("co.fourth.tuna.domain.lectureplan.mapper.LecturePlanMapper.selectSc", vo.getSbjNo());
-		
-//		System.out.println(listz.get(0).toString());
-		
-		String[] plan = lists.get(0).values().toString().split(",");
-		
-		String[] weekplan = plan[11].split("~");
-		
-		for(int i =0; i<plan.length;i++) {
-			System.out.println(plan[i]);
-		}
-		model.addAttribute("sc", lectureSchedule.get(0));
-		model.addAttribute("plan", lists.get(0));
-		model.addAttribute("weekplan", weekplan);
-		return "eclass/stud/lecturePlan";
-	}
+	
 	
 	@PostMapping("/prof/lecPlans")
 	@ResponseBody
