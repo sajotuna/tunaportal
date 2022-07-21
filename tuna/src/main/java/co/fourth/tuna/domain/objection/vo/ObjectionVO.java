a4845ef7997a866268b026354141a4a7f95331f2
Binggla <wlqls12@naver.com>
2022-07-21 19:33:21 +0900

    과목별 성적 이의신청(교수) 조회, 답변 등록 작업 중

--- tuna/src/main/java/co/fourth/tuna/domain/objection/vo/ObjectionVO.java
+++ tuna/src/main/java/co/fourth/tuna/domain/objection/vo/ObjectionVO.java
@@ -4,11 +4,14 @@ import java.sql.Date;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import co.fourth.tuna.domain.grade.vo.GradeVO;
+import co.fourth.tuna.domain.user.vo.StudentVO;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class ObjectionVO {
 	private int no;
@@ -21,6 +24,9 @@ public class ObjectionVO {
 	private String seasonCode;
 	private int stNo;
 	private int sbjNo;
+	
+	GradeVO grade;
+	StudentVO student;
 	
 	private String objState;
 }
--- tuna/src/main/java/co/fourth/tuna/domain/objection/web/ObjectionController.java
+++ tuna/src/main/java/co/fourth/tuna/domain/objection/web/ObjectionController.java
@@ -7,11 +7,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import co.fourth.tuna.domain.banner.web.BannerController;
 import co.fourth.tuna.domain.common.service.YearService;
 import co.fourth.tuna.domain.objection.service.ObjectionService;
 import co.fourth.tuna.domain.objection.vo.ObjectionVO;
@@ -19,10 +20,16 @@ import co.fourth.tuna.domain.objection.vo.ObjectionVO;
 @RestController
 public class ObjectionController {
 	
-	private static Logger logger = LoggerFactory.getLogger(BannerController.class); 
+	private static Logger logger = LoggerFactory.getLogger(ObjectionController.class); 
 	
 	@Autowired ObjectionService objectionDao;
 	@Autowired YearService yearDao;
+	
+	@PostMapping("/prof/objections")
+	public List<ObjectionVO> objectionListBySubjectId(
+			@RequestBody Map<String, Integer>reqData) {
+		return objectionDao.findListBySubjectId(reqData.get("sbjno"));
+	}
 	
 	@GetMapping("/portal/student/objectionList")
 	public List<ObjectionVO> objectionListSelect(int stNo) {
@@ -44,7 +51,7 @@ public class ObjectionController {
 		return objectionDao.objectionListOfProf(pfNo, seasonCode);
 	}
 	
-	@GetMapping("/eclass/professor/objection")
+	@RequestMapping("/eclass/professor/objection")
 	public Map<String, Object> objectionSelect(int objNo) {
 		return objectionDao.objectionSelect(objNo);
 	}
