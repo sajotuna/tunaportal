455795ecad1bcb24b7d0d954db1b1d37a5a11aa8
Binggla <wlqls12@naver.com>
2022-07-24 01:03:26 +0900

    주석 변경

--- tuna/src/main/java/co/fourth/tuna/domain/common/service/FileService.java
+++ tuna/src/main/java/co/fourth/tuna/domain/common/service/FileService.java
@@ -3,7 +3,12 @@ package co.fourth.tuna.domain.common.service;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface FileService {
-
+	
+	/**
+	 * @brief 파일 업로드
+	 * @param MultipartFile file, String folder
+	 * @return [originName(실제 파일명), fileName(변환되어 저장된 파일명)]
+	 */
 	public String[] upload(MultipartFile file, String folder);
 	public void delete(String fileName, String folder);
 	
