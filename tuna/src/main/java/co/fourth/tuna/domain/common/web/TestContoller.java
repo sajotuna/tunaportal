9062ee398f9562576af7a3198724fb20cb6ed982
권가희 <@LAPTOP-3S4CEFMV>
2022-07-24 11:51:54 +0900

    에디터 수정중 ..

--- tuna/src/main/java/co/fourth/tuna/domain/common/web/TestContoller.java
+++ tuna/src/main/java/co/fourth/tuna/domain/common/web/TestContoller.java
@@ -1,5 +1,9 @@
 package co.fourth.tuna.domain.common.web;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import co.fourth.tuna.domain.banner.web.BannerController;
 import co.fourth.tuna.domain.common.service.FileService;
@@ -48,5 +53,35 @@ public class TestContoller {
 	@RequestMapping("/dialogTest")
 	public String dialogTest() {
 		return "test/dialogTest";
+	}
+	
+	//ckeditor5
+	@RequestMapping("/ckTest")
+	public String ckTest() {
+		return "test/ckeditor5Test";
+	}
+	
+	//CKEditor 이미지 업로드
+	@RequestMapping("/editorImage")
+	@ResponseBody
+	public Map<String, String> image(MultipartHttpServletRequest request)
+			throws Exception {
+
+		List<MultipartFile> imageList = request.getFiles("upload");
+		Map<String, MultipartFile> files = request.getFileMap();
+		Map<String, String> url = new HashMap<>();
+		
+		for(int i=0; i<files.size(); i++) {
+			String[] fileInfo = fileService.upload(imageList.get(i), "notice");
+			url.put(String.valueOf(i), fileInfo[1]);
+		}
+		
+		return url;
+		
+	}
+	
+	@RequestMapping("/editorUpload")
+	public void serverUpload() {
+		
 	}
 }
